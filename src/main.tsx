5ec4a5ba4779bba471c241cb9ddf8bc05bf597bc
sui <96000378+otctoken@users.noreply.github.com>
2025-07-03 11:06:46 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// src/App.tsx
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import AA from "./pages/AA";
+import BB from "./pages/BB";
+import CC from "./pages/CC";
+import DD from "./pages/DD";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 p-8 container mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AA" element={<AA />} />
+          <Route path="/BB" element={<BB />} />
+          <Route path="/CC" element={<CC />} />
+          <Route path="/DD" element={<DD />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
