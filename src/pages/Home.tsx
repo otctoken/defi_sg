a4f2b64ff9807617bdf78990cc7b6c29e04d8ae1
sui <96000378+otctoken@users.noreply.github.com>
2025-09-08 09:38:03 +0800

    feat: add My Account section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { useCurrentAccount } from "@mysten/dapp-kit";
 import { Link } from "react-router-dom";
 
 const items = ["AA", "BB", "CC", "DD"] as const;
@@ -7,8 +8,20 @@ const items = ["AA", "BB", "CC", "DD"] as const;
 type ItemID = (typeof items)[number];
 
 export default function Home() {
+  const account = useCurrentAccount();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <Card className="flex flex-col justify-between items-center text-center h-64 !bg-gray-800 sm:col-span-2">
+        <CardHeader>My Account</CardHeader>
+        <CardContent className="w-full flex justify-center">
+          {account ? (
+            <div className="break-all">{account.address}</div>
+          ) : (
+            <div>未连接</div>
+          )}
+        </CardContent>
+      </Card>
       {items.map((id: ItemID) => (
         <Card
           key={id}
