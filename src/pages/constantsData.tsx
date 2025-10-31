fe47c8c95f8ff4ace24ac61ae6cd78375228addf
otctoken <96000378+otctoken@users.noreply.github.com>
2025-10-31 09:38:36 +0000

    25.10.31.1838

---
+++ src/pages/constantsData.tsx
@@ -0,0 +1,60 @@
+// src/constants/A.tsx
+//.................................SG................................................................
+export const SG_PACkages = "0xf8d8e5c5fb84d09c7047551e1085086b028fe4552a9cfe6281b5abc7fed968db"
+export const SG_Amindcap_fee = "0xc9d3223d7c4b758d71a56de6851c6e692959a2ed8126a3835cf4ede3f3a41b1c"
+export const SGC_h_c = "0x5573024b3108e0561b34dcbaa2764104f2c93de1e0ff0aa0dbdd6138792089d4"
+export const SGCcoin_type = "0xf8d8e5c5fb84d09c7047551e1085086b028fe4552a9cfe6281b5abc7fed968db::sgc::SGC"
+
+// export const SG_deep_data = "0x3dc652f26c723a36cfa106fa6df0f21976736e4b90caf5c00ddd03ba744a2fe7"
+// export const SG_deep_Reward_coin_type = "0x268b58ccecc7a2747dcbb83f3f1e70b2b00d22118a6b489b6ffc9cd01af5242b"
+// export const SG_deep_get_sgc = "0x1cae79b75c73891dd9655b0991dad8215bb626ff9511c8804bba18211856f260"
+export const SUI_30H = {
+    // tree_height total_balance time_per_round  start_time index internal_node  leaf_node
+    data: "0xdd8d818191b5f6def973099906846f5dd316ab9ec9c85cb130b4cdcc82c6e90a",
+    reward_coin_type: "0x4224a735430b43192a7b0c40a1689b2d408dfc6deae472dc5fd4177d4a0f0851",
+    get_sgc: "0x83c37f5e0325792a95c933435f1c3c3c910a633639ca56b2266df0d7bdd8abdb",
+    coin_balance_number: 0,
+    time_per_round: 108000000,
+    navi_pool_number: 0,
+    dynamic_field: {
+        adder_node: '0xe977dd54b1197b388644a4bc3146a6404a5a821fcf8232e9261743b762c677fe',
+        internal_node_data: '0xe7f61c0349603dbebe21a391e39de6f0b57bdb258ec5ab225450f2519777fc4f',
+        leaf_node_data: '0x3de95374ded86be9fd8bac75fc994f10bd663d311b157b150b428c36eaa06e5b',
+        node_adder: '0x10e207e9a49f11629c3955dfa7694a0ee60782f971047482878e5e9263b15f8e',
+        savings: '0xe7e139e039b866dd11f2ac70a9fe27383cd053a46c202eec39e52cc5320b6843'
+    }
+}
+export const DEEP_30H = {
+    // tree_height total_balance time_per_round  start_time index internal_node  leaf_node
+    data: "0x6f765c76240d0a4df308897ccc337bd25e9fc06bb18fd3ab5487414c8f2c0781",
+    reward_coin_type: "0x61b13e0ce936504f98838cc9a1c62859975f0bad012afb6f2645dc50f574f249",
+    get_sgc: "0x2efe9ccf8ec8555b22aa575241d2461d35e8522e80626ba461a5ca3e4e0ae630",
+    coin_balance_number: 1,
+    time_per_round: 108000000,
+    navi_pool_number: 15,
+    dynamic_field: {
+        adder_node: '0x7034023f187fd3d5fcab26a57a6f5130bff18095f7c734a66467c09d2b160c48',
+        internal_node_data: '0xedf53df1c3a078e644ee63af4388c226a56133e08fc322ddb17c2193b384f975',
+        leaf_node_data: '0x4279cef4fa8bc85dde35489dd1d9c048317ba3db06d5e4ed599a6eae2ae017b2',
+        node_adder: '0x6338e4911c57f72a8130346c45e286c5a92ba5f4586b3dc3cb571cb8a9b9d8e0',
+        savings: '0x2a078e90e69bba435e3941595d59ec7eafe26f67c1e3c4651c71f1e87c73bf6a'
+    }
+}
+
+//.................................NAVI................................................................
+export const Navi_Storage = "0xbb4e2f4b6205c2e2a2db47aeb4f830796ec7c005f88537ee775986639bc442fe"
+export const Navi_inshdg_V2 = "0xf87a8acb8b81d14307894d12595541a73f19933f88e1326d5be349c7a6f7559c"
+export const Navi_inshdg_V3 = "0x62982dad27fb10bb314b3384d5de8d2ac2d72ab2dbeae5d801dbdb9efa816c80"
+export const Navi_RewadFun_NAVI = "0x1a3f9fcfdfac10e92c99220203f7c4bb502558692f0be0f2cb5f788b4e12a6b5"
+export const Navi_RewadFun_Vsui = "0x7093cf7549d5e5b35bfde2177223d1050f71655c7f676a5e610ee70eb4d93b5c"
+export const Navi_PriceOracle = "0x1568865ed9a0b5ec414220e8f79b3d04c77acc82358f6e5ae4635687392ffbef"
+export const Navi_pool_A_sui = "0x96df0fce3c471489f4debaaa762cf960b3d97820bd1f3f025ff8190730e958c5"
+
+//.................................FolX................................................................
+export const FolX_Contert = "0xb65dcbf63fd3ad5d0ebfbf334780dc9f785eff38a4459e37ab08fa79576ee511"
+export const FolX_Mingter = "0x2a30f1bfa9594e0e9ca37581268b3fee4b6965a70c38eb76e204334980619c24"
+//..............................admin...................................................................
+
+export const AdminCap = "0x7eeff60c58790639cf8796ceb38bac7f4b4d0d97cf44ccaff73cf8a7896d49b3"
+export const upgrade = "0x59850fdd02e32410a4abf21ca0ac375648a450fabfc6581f0100ccadb088cf47"
+export const SGC_AdminTolel = "0x5bfcf8d401970a0db6386d8504a6b4177fc3745638e47763cc52e74b2cb10d88"//
